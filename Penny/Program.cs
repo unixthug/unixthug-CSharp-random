c3ad9bf297bf4eae47800ade7a2a9f5e2fcfceec
Adam Jones <106862447+unixthug@users.noreply.github.com>
2023-09-07 16:33:58 -0500

    Add files via upload

---
+++ Penny/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+namespace Program
+{
+class Program
+{
+    public static void Main(string[] args)
+    {
+        //count your coins
+        Console.WriteLine("How many quarters do you have?");
+        double q = Convert.ToDouble(Console.ReadLine());
+        Console.WriteLine("How many dimes do you have?");
+        double d = Convert.ToDouble(Console.ReadLine());
+        Console.WriteLine("How many nickels do you have?");
+        double n = Convert.ToDouble(Console.ReadLine());
+        Console.WriteLine("How many pennies do you have?");
+        double p = Convert.ToDouble(Console.ReadLine());
+        
+        //convert to cents
+        q *= 0.25;
+        d *= 0.10;
+        n *= 0.05;
+        p *= 0.01;
+
+        //print result
+        Console.WriteLine($"You have ${q + d + n + p} dollars!");
+
+
+    }
+}
+}
